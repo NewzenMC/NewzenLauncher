bdc5979efd637cc64bc229e8112228aea66659e5
Titouan Petit <petit.titouan1@gmail.com>
2020-10-21 22:06:23 +0200

    Added Admin Panel Button
    Only showed if you are in the authorized list

--- app/assets/js/scripts/adminPanel.js
+++ app/assets/js/scripts/adminPanel.js
@@ -4,15 +4,24 @@ function refreshAdminPanelAccess() {
             url: 'http://tieb62.freeboxos.fr/authorized/' + account,
             success: response => {
                 if (response === 'true') {
-                    showAdminPanelButton()
+                    $('#adminPanelBtn').fadeIn(200)
+                } else {
+                    $('#adminPanelBtn').fadeOut(200)
                 }
             }
         })
     }
 }
 
-function showAdminPanelButton() {
-    //TODO: Afficher Admin Panel Button
+$('#adminPanelBtn').on('click', () => {
+    switchView(getCurrentView(), VIEWS.adminPanel)
+})
+
+// Escape key return to main menu
+document.onkeyup = (e) => {
+    if (e.key === 'Escape' && getCurrentView() === VIEWS.adminPanel) { // escape key maps to keycode `27` 
+        switchView(getCurrentView(), VIEWS.landing)
+    }
 }
 
 refreshAdminPanelAccess()
