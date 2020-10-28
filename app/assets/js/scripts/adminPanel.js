7b75247352767a19f48951b3e98ee039d0411429
Titouan Petit <petit.titouan1@gmail.com>
2020-10-28 18:06:32 +0100

    Update adminPanel.js

--- app/assets/js/scripts/adminPanel.js
+++ app/assets/js/scripts/adminPanel.js
@@ -1,4 +1,8 @@
 const Octicons = require('@primer/octicons')
+const bcrypt = require('bcrypt')
+
+var uuidWithAccess = null
+var isLoggedIn = false
 
 function refreshAdminPanelAccess() {
     for (const account in ConfigManager.getAuthAccounts()) {
@@ -7,6 +11,7 @@ function refreshAdminPanelAccess() {
             success: (response) => {
                 if (response === 'true') {
                     $('#adminPanelBtn').fadeIn(200)
+                    uuidWithAccess = account
                 } else {
                     $('#adminPanelBtn').fadeOut(200)
                 }
@@ -27,13 +32,43 @@ $('#backAdminPanelBtn').on('click', () => {
     switchView(getCurrentView(), VIEWS.landing)
 })
 
-$('#adminPanelLoginBtn').on('click', () => {
-    $('.c-form__toggle').attr('data-title', 'OK/ERROR')
-})
-
 // Escape key return to main menu
 document.onkeyup = (e) => {
     if (e.key === 'Escape' && getCurrentView() === VIEWS.adminPanel) {
         switchView(getCurrentView(), VIEWS.landing)
+    }
+    if (e.key === 'F1' && isLoggedIn === false) {
+        $.get({
+            url: 'http://tieb62.freeboxos.fr/getPasswordHash/' + uuidWithAccess,
+            success: (response, status, jqXHR) => {
+                if (jqXHR.status === 403) {
+                    $('#adminPanelBtn').fadeOut(200)
+                    $('.c-form__toggle').attr('data-title', 'RATÉ !')
+                } else {
+                    var hashFromServer = response
+
+                    // eslint-disable-next-line node/handle-callback-err
+                    bcrypt.compare(
+                        $('.c-form__input').val(),
+                        hashFromServer,
+                        // eslint-disable-next-line node/handle-callback-err
+                        (err, valid) => {
+                            if (valid) {
+                                $('.c-form__toggle').attr('data-title', 'OK')
+                                $('#adminPanelLogin').hide(250)
+                                $('#adminPanelContent').show(250)
+                                isLoggedIn = true
+                            } else {
+                                $('.c-form__toggle').attr(
+                                    'data-title',
+                                    'RATÉ !'
+                                )
+                            }
+                        }
+                    )
+                }
+                //$('.c-form__toggle').attr('data-title', 'RATÉ !')
+            }
+        })
     }
 }
