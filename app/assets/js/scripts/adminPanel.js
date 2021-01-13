7d45884abfea6190c09ce121d24f5cdb612e6286
Titouan Petit <petit.titouan1@gmail.com>
2021-01-13 19:16:37 +0100

    Added Authentication System to the Admin Panel

--- app/assets/js/scripts/adminPanel.js
+++ app/assets/js/scripts/adminPanel.js
@@ -4,23 +4,29 @@ const bcrypt = require('bcrypt')
 let uuidWithAccess = null
 let isLoggedIn = false
 
-function refreshAdminPanelAccess() {
-    for (const account in ConfigManager.getAuthAccounts()) {
-        $.get({
-            url: 'http://tieb62.freeboxos.fr/authorized/' + account,
-            success: (response) => {
-                if (response === 'true') {
-                    $('#adminPanelBtn').fadeIn(200)
-                    uuidWithAccess = account
-                } else {
-                    $('#adminPanelBtn').fadeOut(200)
-                }
-            }
-        })
-    }
-}
-refreshAdminPanelAccess()
-//NEWFEATURE Refresh dès que l'utilisateur ajoute ou supprime un compte du launcher
+$('#sendDiscordConfirmBtn:enabled').on('click', () => {
+    let btn = $('#sendDiscordConfirmBtn')
+    btn.attr('disabled', 'disabled')
+    btn.text('Vérification...')
+    socket.emit('authRequest', null)
+    socket.on('authResponse', (response) => {
+        if (response) {
+            btn.text('Accès Autorisé !')
+            setTimeout(() => {
+                btn.removeAttr('disabled')
+                btn.text('Vérifier mon Identité')
+                $('#adminPanelLogin').fadeOut(500)
+                $('#adminPanelContent').fadeIn(500)
+            }, 2000)
+        } else {
+            btn.text('Accès Refusé !')
+            setTimeout(() => {
+                btn.removeAttr('disabled')
+                btn.text('Vérifier mon Identité')
+            }, 2000)
+        }
+    })
+})
 
 $('#adminPanelBtn').on('click', () => {
     switchView(getCurrentView(), VIEWS.adminPanel)
@@ -31,44 +37,3 @@ $('#backAdminPanelSVG').html(Octicons['chevron-left'].toSVG())
 $('#backAdminPanelBtn').on('click', () => {
     switchView(getCurrentView(), VIEWS.landing)
 })
-
-// Escape key return to main menu
-document.onkeyup = (e) => {
-    if (e.key === 'Escape' && getCurrentView() === VIEWS.adminPanel) {
-        switchView(getCurrentView(), VIEWS.landing)
-    }
-    if (e.key === 'F1' && isLoggedIn === false) {
-        $.get({
-            url: 'http://tieb62.freeboxos.fr/getPasswordHash/' + uuidWithAccess,
-            success: (response, status, jqXHR) => {
-                if (jqXHR.status === 403) {
-                    $('#adminPanelBtn').fadeOut(200)
-                    $('.c-form__toggle').attr('data-title', 'RATÉ !')
-                } else {
-                    let hashFromServer = response
-
-                    // eslint-disable-next-line node/handle-callback-err
-                    bcrypt.compare(
-                        $('.c-form__input').val(),
-                        hashFromServer,
-                        // eslint-disable-next-line node/handle-callback-err
-                        (err, valid) => {
-                            if (valid) {
-                                $('.c-form__toggle').attr('data-title', 'OK')
-                                $('#adminPanelLogin').hide(250)
-                                $('#adminPanelContent').show(250)
-                                isLoggedIn = true
-                            } else {
-                                $('.c-form__toggle').attr(
-                                    'data-title',
-                                    'RATÉ !'
-                                )
-                            }
-                        }
-                    )
-                }
-                //$('.c-form__toggle').attr('data-title', 'RATÉ !')
-            }
-        })
-    }
-}
