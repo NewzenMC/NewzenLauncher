9dd25e935b1157380ab8dfca08dbda57c8b9a258
Titouan Petit <petit.titouan1@gmail.com>
2021-01-14 21:45:04 +0100

    Replaced AdminPanel Tabs

--- app/assets/js/scripts/adminPanel.js
+++ app/assets/js/scripts/adminPanel.js
@@ -36,3 +36,58 @@ $('#backAdminPanelSVG').html(Octicons['chevron-left'].toSVG())
 $('#backAdminPanelBtn').on('click', () => {
     switchView(getCurrentView(), VIEWS.landing)
 })
+
+//#region Tabs
+let TabBlock = {
+    s: {
+        animLen: 200
+    },
+
+    init: function () {
+        TabBlock.bindUIActions()
+        TabBlock.hideInactive()
+    },
+
+    bindUIActions: function () {
+        $('.tabBlock-tabs').on('click', '.tabBlock-tab', function () {
+            TabBlock.switchTab($(this))
+        })
+    },
+
+    hideInactive: function () {
+        let $tabBlocks = $('.tabBlock')
+
+        $tabBlocks.each(function (i) {
+            let $tabBlock = $($tabBlocks[i])
+            let $panes = $tabBlock.find('.tabBlock-pane')
+            let $activeTab = $tabBlock.find('.tabBlock-tab.is-active')
+
+            $panes.hide()
+            $($panes[$activeTab.index()]).show()
+        })
+    },
+
+    switchTab: function ($tab) {
+        let $context = $tab.closest('.tabBlock')
+
+        if (!$tab.hasClass('is-active')) {
+            $tab.siblings().removeClass('is-active')
+            $tab.addClass('is-active')
+
+            TabBlock.showPane($tab.index(), $context)
+        }
+    },
+
+    showPane: function (i, $context) {
+        let $panes = $context.find('.tabBlock-pane')
+
+        // Normally I'd frown at using jQuery over CSS animations, but we can't transition between unspecified variable heights, right? If you know a better way, I'd love a read it in the comments or on Twitter @johndjameson
+        $panes.slideUp(TabBlock.s.animLen)
+        $($panes[i]).slideDown(TabBlock.s.animLen)
+    }
+}
+
+$(function () {
+    TabBlock.init()
+})
+//#endregion Tabs
