35fc386d47e1ec4cb2ae78bfad28b4c2f9d426b8
Titouan Petit <petit.titouan1@gmail.com>
2021-02-03 20:47:55 +0100

    Update socketioManager.js
    Ajout d'un overlay bloquant le launcher si il n'est pas connecté au NewzenManager

--- app/assets/js/socketioManager.js
+++ app/assets/js/socketioManager.js
@@ -1,10 +1,22 @@
 const socket = require('socket.io-client')('http://tieb62.freeboxos.fr:8080')
+setTimeout(() => {
+    refreshNoDatacenterConnectionOverlay()
+}, 5000)
 const Maintenance = require('./assets/js/maintenance')
 
 let permissionLevel = 0
+let datacenterConnection = false
+let datacenterConnectionInterval = null
 
 socket.on('connect', () => {
     socket.send(Object.keys(ConfigManager.getAuthAccounts())[0])
+    datacenterConnection = true
+    refreshNoDatacenterConnectionOverlay()
+})
+
+socket.on('disconnect', () => {
+    datacenterConnection = false
+    refreshNoDatacenterConnectionOverlay()
 })
 
 socket.on('message', (data) => {
@@ -25,3 +37,28 @@ socket.on('maintenance', (status) => {
         Maintenance.disableMaintenance()
     }
 })
+
+function refreshNoDatacenterConnectionOverlay() {
+    if (!datacenterConnection) {
+        setOverlayContent(
+            'Connection aux Serveurs de Données Impossible',
+            'Il est actuellement impossible de se connecter aux Serveurs de Données de Newzen<br>Vérifiez votre connection Internet !<br>Si le problème persiste, veuillez le signaler à TIEB62#3087 sur le Discord de Newzen !',
+            'Rejoindre le Discord'
+        ) //TODO Mettre un lien vers la page de status de Newzen
+        setOverlayHandler(() => {
+            require('electron').shell.openExternal(
+                'https://tieb62.freeboxos.fr/discord'
+            )
+        })
+        $('#main').fadeOut()
+        datacenterConnectionInterval = setInterval(() => {
+            toggleOverlay(true)
+        }, 100)
+        toggleOverlay(true)
+    } else {
+        clearInterval(datacenterConnectionInterval)
+        setOverlayHandler(null)
+        toggleOverlay(false)
+        $('#main').fadeIn()
+    }
+}
